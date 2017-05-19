c91e37edb84b3edbcb843451969ad7720108af29
kan5787 <kan5787@sina.com>
2017-05-19 18:06:02 +0800

    空格返回空字符

--- FnMain.js
+++ FnMain.js
@@ -1,3 +1,8 @@
 /**
  * Created by Jolene on 2017/5/19.
  */
+function main() {
+    return ''
+}
+
+module.exports=main;
--- spec/FnSpec.js
+++ spec/FnSpec.js
@@ -1,3 +1,11 @@
 /**
  * Created by Jolene on 2017/5/19.
  */
+var main=require('../FnMain.js');
+
+describe("Word Frequency",function(){
+    it("returns empty string given string",function () {
+        var result=main('');
+        expect(result).toEqual('');
+    });
+});
