74641c92fcc981c45ecb09fba000caabfb978504
kan5787 <kan5787@sina.com>
2017-05-19 19:15:21 +0800

    重新设定重复部分

--- FnMain.js
+++ FnMain.js
@@ -6,11 +6,8 @@ var format=function(word,count){
 }
 function main(words) {
     if(words !==''){
-        if (words.indexOf(' '>-1)){
-            let wordArray=words.split(' ');
-            return format(wordArray[0],1)+'\r\n'+format(wordArray[1],1);
-        }
-        return format(words,1);
+        let wordArray=words.split(' ');
+        return wordArray.map((w)=>format(w,1)).join('\r\n');
     }
     return ''
 }
